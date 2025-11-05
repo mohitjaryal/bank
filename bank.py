280be16559364ed8d6da707451577032f7fbb71b
Mohit <mohitt.tech@gmail.com>
2025-11-05 12:55:13 +0530

    Update bank.py

--- bank.py
+++ bank.py
@@ -5,3 +5,13 @@ def create_acc():
     name = input('Enter your name :')
     balance = float('Enter initial balance :')
     print('Account created Sucessfully fror {name} with {balance} initial balance')
+    return {'name': name,'balance':balance }
+
+# Debit
+def debit():
+    amount = float(input('Enter amount to deposit :'))
+    amount['balance'] += amount
+    print(f"₹{amount:.2f} deposited successfully!")
+    print(f"New Balance: ₹{account['balance']:.2f}")
+
+# Cre

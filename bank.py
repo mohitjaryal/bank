32cefceef00eacc72cb80973bfbc8687d233935c
Mohit <mohitt.tech@gmail.com>
2025-11-05 12:52:06 +0530

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,2 +1,7 @@
 # Banking system made with python
 
+# Create Account
+def create_acc():
+    name = input('Enter your name :')
+    balance = float('Enter initial balance :')
+    print('Account created Sucessfully fror {name} with {balance} initial balance')
